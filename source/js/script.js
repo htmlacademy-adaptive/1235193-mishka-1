0bb5685647bd6142cc825e037422b302b682408e
Elena Popova <ep4360619@gmail.com>
2023-10-17 13:28:25 +0300

    проверяет индекс пиксель перфект

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,26 @@
-const productCart = document.querySelector('.product__cart');
+const productCarts= document.querySelectorAll('.product__cart');
 const modal = document.querySelector('.modal');
 const modalButton = document.querySelector('.modal__button');
+const nav = document.querySelector('.main-nav');
+const menuButton = document.querySelector('.main-nav__toggle');
 
-productCart.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  modal.classList.remove('modal--close');
-});
+nav.classList.add('main-nav--js');
+nav.classList.add('main-nav--closed');
 
-modalButton.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  modal.classList.add('modal--close');
-});
+menuButton.addEventListener('click', () => {
+nav.classList.toggle('main-nav--closed');
+})
+
+if (modal) {
+  productCarts.forEach((item) => {
+    item.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      modal.classList.remove('modal--close');
+    });
+  })
+
+  modalButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    modal.classList.add('modal--close');
+  });
+}
